2d9e05ad61322c01942c3fd109b3d4935660cbfe
Tynda <tyndareus1274@gmail.com>
2023-04-27 13:56:35 +0200

    delete fix

--- src/Sections/Messages/MessagesList.jsx
+++ src/Sections/Messages/MessagesList.jsx
@@ -1,22 +1,11 @@
 import { Card, Button } from 'react-bootstrap'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, deleteOneDoc } from '../../components/firebase/api'
-import { useState } from 'react'
 
 export const CommentList = ({ messages }) => {
-  const [allMessages, setAllMessages] = useState(messages)
   const [userLogin] = useAuthState(auth)
 
-  const deleteMessage = (id) => {
-    for (let index = 0; index < allMessages.length; index++) {
-      if (messages[index].id === id) {
-        messages.splice(index, 1)
-      }
-    }
-    setAllMessages(messages)
-    window.location.reload(true)
-  }
-  return allMessages.map(({ name, mail, date, commentText, id, isNew }) => (
+  return messages.map(({ name, mail, date, commentText, id, isNew }) => (
     <Card bg="light" className="mt-1" key={date}>
       <Card.Header>From: {name}</Card.Header>
       <Card.Body>
@@ -27,15 +16,21 @@ export const CommentList = ({ messages }) => {
         ? null
         : (
         <Button
-        disabled={isNew}
-          onClick={() => {
-            deleteOneDoc(id)
-            deleteMessage(id)
+          disabled={isNew}
+          onClick={async () => {
+            try {
+              deleteOneDoc(id)
+            } catch (error) {
+              alert(error)
+            }
+            setTimeout(() => {
+              window.location.reload(true)
+            }, 3000)
           }}
           variant="danger"
           style={{ margin: 'auto', marginTop: 2, marginBottom: 2, width: 140 }}
         >
-          {isNew ? 'Reload for delete' : 'Delete' }
+          {isNew ? 'Reload for delete' : 'Delete'}
         </Button>
           )}
     </Card>
